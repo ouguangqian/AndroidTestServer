c7d2f7f69db089878efb0d5d4eaad0befb3ba703
Guangqian.Ou <ou.guangqian@hotmail.com>
2018-03-10 18:08:01 +0800

    添加基本操作

--- app/src/androidTest/java/server/RpcServiceImpl.java
+++ app/src/androidTest/java/server/RpcServiceImpl.java
@@ -2,20 +2,14 @@ package server;
 
 import android.support.test.InstrumentationRegistry;
 import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiSelector;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class RpcServiceImpl implements IRpcService {
 
-        final UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-    public String logMsg() {
-        System.out.println("No Arguments log");
-        return "No Argument";
-    }
-
-    public String logMsg(String msg) {
-        System.out.println("With Arguments log");
-
-        return "With Argument >>>" + msg;
-    }
+    final UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
 
     @Override
     public void home() {
@@ -33,8 +27,50 @@ public class RpcServiceImpl implements IRpcService {
     }
 
     @Override
-    public void click(int x, int y) {
+    public void clickByCoordinate(int x, int y) throws Exception {
         uiDevice.click(x, y);
+
     }
 
+    @Override
+    public void clickById(String resourceId) throws Exception {
+        uiDevice.findObject(new UiSelector().resourceId(resourceId)).click();
+    }
+
+    @Override
+    public void clickByText(String text) throws Exception {
+        uiDevice.findObject(new UiSelector().text(text));
+
+    }
+
+    @Override
+    public void clickByTextContains(String text) throws Exception {
+        uiDevice.findObject(new UiSelector().textContains(text)).click();
+
+    }
+
+    @Override
+    public void verifyTextExists(String text) throws Exception {
+        assertTrue(uiDevice.findObject(new UiSelector().textContains(text)).exists());
+
+    }
+
+    @Override
+    public void verifyTextNotExists(String text) throws Exception {
+        assertFalse(uiDevice.findObject(new UiSelector().textContains(text)).exists());
+
+    }
+
+    @Override
+    public void verifyEleExists(String resourceId) throws Exception {
+        assertTrue(uiDevice.findObject(new UiSelector().resourceId(resourceId)).exists());
+
+
+    }
+
+    @Override
+    public void verifyEleNotExists(String resourceId) throws Exception {
+        assertFalse(uiDevice.findObject(new UiSelector().resourceId(resourceId)).exists());
+
+    }
 }
